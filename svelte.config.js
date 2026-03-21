8a457c5d04407724a830c611022286361e3e4ff6
Skydev125 <diogo_goncalves2004@hotmail.com>
2026-03-21 07:25:37 +0000

    fix: edit env variable

--- svelte.config.js
+++ svelte.config.js
@@ -3,17 +3,7 @@ import adapter from '@sveltejs/adapter-static';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
     kit: {
-        adapter: adapter({
-            // Creates a custom 404 page for GitHub Pages
-            fallback: '404.html'
-        }),
-        prerender: {
-            handleHttpError: ({ path, referrer, message }) => {
-                console.log('HTTP Error during prerender:', { path, referrer, message });
-                if (path === '/') return 'ignore'; // Suppress error for '/'
-                throw new Error(message);
-            }
-        }
+        adapter: adapter()
     },
     vitePlugin: {
         dynamicCompileOptions: ({ filename }) =>
